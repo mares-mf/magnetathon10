b33e5b977c594e7ecee6d40eebea3004def9067b
mares <Mariel.Martinez@magnetforensics.com>
2020-12-16 18:57:04 -0500

    Add cloudfront resources to terraform

--- devops/terraform/output.tf
+++ devops/terraform/output.tf
@@ -1,3 +1,7 @@
-output "magnetathon10" {
+output "S3_Bucket" {
   value = aws_s3_bucket.magnetathon10.website_endpoint
+}
+
+output "CloudFront_Domain" {
+  value = aws_cloudfront_distribution.s3_distribution.domain_name
 }
