a0afa147e8381b6e3d9b030015217f22803d8cbf
Michael Kirkpatrick <Michael.Kirkpatrick@magnetforensics.com>
2020-12-16 14:01:33 -0500

    Base nextjs components

---
+++ ui/magnetathon/pages/ContentType/ContentA.js
@@ -0,0 +1,14 @@
+﻿import Link from 'next/link'
+
+export default function ContentA() {
+    return (
+        <>
+            <h1>Some categorized content!</h1>
+            <h2>
+                <Link href="/">
+                    <a>Home</a>
+                </Link>
+            </h2>
+        </>
+    )
+}
