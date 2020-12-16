b87a32e27542b76c702af964c58efe1f51d86fec
mares <Mariel.Martinez@magnetforensics.com>
2020-12-16 15:33:57 -0500

    Add content type to objects

--- devops/terraform/main.tf
+++ devops/terraform/main.tf
@@ -21,6 +21,18 @@ resource "aws_s3_bucket" "magnetathon10" {
   }
 }
 
+variable "mime_types" {
+  default = {
+    htm   = "text/html"
+    html  = "text/html"
+    css   = "text/css"
+    ttf   = "font/ttf"
+    js    = "application/javascript"
+    map   = "application/javascript"
+    json  = "application/json"
+  }
+}
+
 #upload everything in dist
 resource "aws_s3_bucket_object" "dist" {
   for_each = fileset("../../ui/magnetathon/out/", "**")
@@ -28,4 +40,5 @@ resource "aws_s3_bucket_object" "dist" {
   key = each.value
   source = "../../ui/magnetathon/out/${each.value}"
   etag = filemd5("../../ui/magnetathon/out/${each.value}")
+  content_type  = lookup(local.mime_types, split(".", each.value)[length(split(".", each.value)) - 1])
 }
