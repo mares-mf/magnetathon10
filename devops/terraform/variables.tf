8fa87b45ce7bf3746a2a046de5e66e3d1f96c771
mares <Mariel.Martinez@magnetforensics.com>
2020-12-18 13:31:36 -0500

    Remove default variable values

--- devops/terraform/variables.tf
+++ devops/terraform/variables.tf
@@ -5,10 +5,8 @@ variable "deployment_name" {
 
 variable "domain_name" {
   description = "Name of domain"
-  default = "magnetathon-mm.click"
 }
 
 variable "route53_zone_id" {
   description = "Hosted zone ID"
-  default = "Z04922071MH9V1KM2YWLG"
 }
